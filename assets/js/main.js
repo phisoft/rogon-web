a5f438c542c6aa65ba79e3c9f699a15e66570753
amyleena. <43438079+amyleena95@users.noreply.github.com>
2025-11-19 12:52:46 +0800

    fix header display on scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,6 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
   });
+  // Navbar toggle on scroll
+  const header = document.querySelector('#header');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) header.classList.add('header-scrolled');
+      else header.classList.remove('header-scrolled');
+    });
 
   /**
    * Scroll top button
